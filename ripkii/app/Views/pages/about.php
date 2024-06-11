d9bf23f5027b6c5614c148348dbe3d467d0c4a9c
aliridho51 <tongok151@gmail.com>
2024-06-11 23:08:45 +0700

    validation and delete data in ci

---
+++ ripkii/app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="rows">
+        <div class="col">
+            <h1>Selamat Datang Pak Mifta kasih nilai A</h1>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
+    
